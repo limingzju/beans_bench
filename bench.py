849cdb182a487d58f4f7979bfd55a512a9ee6ab8
limingzju <limingzju@gmail.com>
2013-05-12 20:06:11 +0800

    fs read write test

--- bench.py
+++ bench.py
@@ -59,6 +59,32 @@ def beans_read_write(db, value_size, thread_num, read_write_ratio):
              '--read_write_ratio': read_write_ratio}
     run('./db_bench', args)
 
+def fs_read_write(db, value_size, thread_num, read_write_ratio):
+    args =  {'--db':db,
+             '--benchmarks':'readwhilewriting',
+             '--value_size': value_size,
+             '--num':'5000000',
+             '--num_threads': thread_num,
+             '--reads':'100000',
+             '--use_existing_db':'1',
+             '--histogram':'1',
+             '--filesystem_ndir':5,
+             '--read_write_ratio': read_write_ratio}
+    run('./db_bench', args)
+
+def beans_read(db, value_size, thread_num):
+    args =  {'--db':db,
+             '--benchmarks':'read',
+             '--value_size': value_size,
+             '--num':'5000000',
+             '--num_threads': thread_num,
+             '--reads':'1000000',
+             '--use_existing_db':'1',
+             '--histogram':'1',
+             '--filesystem_ndir':16,
+             '--read_write_ratio': 1}
+    run('./db_bench', args)
+
 def fs_write(value_size, thread_num):
     # 500G
     args =  {'--db': 'fdb',
@@ -69,7 +95,7 @@ def fs_write(value_size, thread_num):
              '--reads':'100000',
              '--use_existing_db':'1',
              '--histogram':'1',
-             '--filesystem_ndir':1000}
+             '--filesystem_ndir':5}
     run('./db_bench', args)
 
 def fs_read(value_size, thread_num):
@@ -79,10 +105,10 @@ def fs_read(value_size, thread_num):
              '--value_size': value_size,
              '--num':'5000000',
              '--num_threads': thread_num,
-             '--reads':'100000',
+             '--reads':'1000000',
              '--use_existing_db':'1',
              '--histogram':'1',
-             '--filesystem_ndir':1000}
+             '--filesystem_ndir':5}
     run('./db_bench', args)
 
 def fs():
@@ -109,22 +135,36 @@ def read_write(db):
     beans_read_write(db, value_size, 6, 5)
     beans_read_write(db, value_size, 6, 0.2)
 
+def fs_read_write_case(db):
+    # 100KB
+    value_size = int(100e3)
+    fs_read_write(db, value_size, 2, 1)
+    fs_read_write(db, value_size, 6, 5)
+    fs_read_write(db, value_size, 6, 0.2)
+
+    # 1MB
+    value_size = int(1e6)
+    fs_read_write(db, value_size, 2, 1)
+    fs_read_write(db, value_size, 6, 5)
+    fs_read_write(db, value_size, 6, 0.2)
+
+    # 5MB
+    value_size = int(5e6)
+    fs_read_write(db, value_size, 2, 1)
+    fs_read_write(db, value_size, 6, 5)
+    fs_read_write(db, value_size, 6, 0.2)
+
 
 def main():
-    if sys.argv[1] == 'beansdb':
-        read_write('beansdb')
-    elif sys.argv[1] == 'fswrite':
-        value_size = int(100e3)
-        fs_write(value_size, 4)
-    elif sys.argv[1] == 'fsread':
+    if sys.argv[1] == 'beans_read':
+        beans_read('beansdb', int(100e3), 4)
+    elif sys.argv[1] == 'fs':
         value_size = int(100e3)
         fs_read(value_size, 4)
-    elif sys.argv[1] == 'fs':
-        fs()
+        fs_read_write_case('fdb')
     else:
         print 'Error'
 
 if __name__ == '__main__':
     main()
-
 
